d09dad9d27caef95cb6695d6187c02d7b091d259
Catie Lanier <hello@catielanier.ca>
2024-10-15 19:17:53 -0400

    create server, initial connection to supabase

---
+++ server.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, send_from_directory
+from flask_cors import CORS
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+# load constants
+load_dotenv()
+SUPABASE_URI: str = os.getenv("SUPABASE_URI")
+SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
+ENVIRONMENT: str = os.getenv("ENVIRONMENT")
+DEBUG: bool = ENVIRONMENT != "prod"
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+supabase: Client = create_client(SUPABASE_URI, SUPABASE_KEY)
+
+if not DEBUG:
+    @app.route("/")
+    def serve_svelte_app():
+        return send_from_directory('dist', 'index.html')
+
+if __name__ == "__main__":
+    app.run()
