9a003d37421550c69b19d47d66436a23984a7c19
Catie Lanier <hello@catielanier.ca>
2024-10-15 18:21:08 -0400

    first actual commit

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	router.Run(":8080")
+}
