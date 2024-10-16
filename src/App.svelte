7f77c5a2ff77e948993064175a45db801cbeba9c
Catie Lanier <hello@catielanier.ca>
2024-10-15 23:55:37 -0400

    setup frontend router

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,33 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import {onMount} from "svelte";
+  import {Router, Route} from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import Update from "./routes/Update.svelte";
+  import Login from "./routes/Login.svelte";
+  import Scoreboard from "./routes/Scoreboard.svelte";
+  import Commentators from "./routes/Commentators.svelte";
+
+  let loggedIn: boolean | null;
+
+  onMount(() => {
+    loggedIn = !!localStorage.getItem("token");
+  })
+
+  export let url: string = "";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<Router {url}>
+  <main>
+    {#if loggedIn}
+      <Route path="/" component={Home} />
+      <Route path="/update" component={Update} />
+    {:else}
+      <Route path="/" component={Login} />
+    {/if}
+    <Route path="/scoreboard" component={Scoreboard} />
+    <Route path="/commentators" component={Commentators} />
+  </main>
+</Router>
 
 <style>
   .logo {
