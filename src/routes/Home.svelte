c7f859de48e1a654525575b8fe07754e2d26f67a
Catie Lanier <hello@catielanier.ca>
2024-10-19 17:55:21 -0400

    add basic navigation

---
+++ src/routes/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+import {Link} from "svelte-routing";
+</script>
+
+<div class="home">
+    <Link to="/scoreboard">Scoreboard</Link>
+    <Link to="/commentators">Commentator Info</Link>
+    <Link to="/update">Update Scoreboard</Link>
+</div>
