9ea9f920e28455db7cfaa76d70e24f5a0b328b2f
Catie Lanier <hello@catielanier.ca>
2024-10-19 18:52:31 -0400

    setup render of data for commentators

--- src/routes/Commentators.svelte
+++ src/routes/Commentators.svelte
@@ -1,5 +1,49 @@
 <script lang="ts">
+    import {onDestroy, onMount} from "svelte";
+    import {io, type Socket} from "socket.io-client";
+    import {retrieveCommentators} from "../utils/api";
     import type {Commentator} from "../utils/types";
 
-    export let commentators: Commentator[];
+    let commentators: Commentator[];
+    let socket: Socket | null;
+
+    onMount(async () => {
+        commentators = await retrieveCommentators();
+
+        socket = io();
+        socket?.on('connect', () => {
+            console.log('connection established')
+        })
+        socket?.on('scoreboard_updated', async () => {
+            commentators = await retrieveCommentators();
+        })
+    })
+
+    onDestroy(() => {
+        socket?.disconnect()
+        console.log('connection closed')
+    })
 </script>
+
+<div class="commentators">
+    {#if commentators?.length}
+        <div class="wrapper">
+            <div class="left-commentator">
+                <div class="commentator-name">
+                    <span class="team-name">{commentators[0].teamName}</span> {commentators[0].commentatorName}
+                </div>
+                <div class="commentator-socials">
+                    @{commentators[0].xHandle}
+                </div>
+            </div>
+            <div class="right-commentator">
+                <div class="commentator-name">
+                    <span class="team-name">{commentators[1].teamName}</span> {commentators[1].commentatorName}
+                </div>
+                <div class="commentator-socials">
+                    @{commentators[1].xHandle}
+                </div>
+            </div>
+        </div>
+    {/if}
+</div>
